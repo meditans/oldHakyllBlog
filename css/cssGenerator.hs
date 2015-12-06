9a59a01a3e5355aa70c7abb3236b8072759ffec3
meditans <meditans@gmail.com>
2015-12-06 02:42:59 +0100

    Comincio a lavorare sul layout che voglio

--- css/cssGenerator.hs
+++ css/cssGenerator.hs
@@ -1,57 +1,83 @@
 {-# LANGUAGE OverloadedStrings #-}
 
 import Clay
+import Clay.Stylesheet (StyleM)
 import Prelude hiding (div)
 
 main :: IO ()
 main = putCss $ do
   body ? do
-    background  base3
     color       base00
-    border      dashed (px 2) orange
     fontSize    (px 18)
     fontFamily [ "Palatino Linotype" , "Book Antiqua"
                , "Palatino" , "FreeSerif" ] [serif]
 
   div # "#header" ? do
+    background  base3
     borderBottom solid (px 2) base01
+    borderLeft solid (px 2) base01
+    borderRight solid (px 2) base01
     marginBottom (px 30)
-    padding (px 20) (px 0) (px 20) (px 0)
+    posizionaOrizzontalmente
 
-  div # "#logo" ? a ? do
-    color base01
-    float floatLeft
-    fontSize (px 30)
-    fontWeight bold
-    textDecoration none
-    
-  div # "#header"
-          # "#navigation" ? do
-            textAlign end
+  div # "#logo" ? do
+    display inline
 
-  div # "#header"
-          # "#navigation" ? a ? do
-            color black
-            fontSize (px 18)
-            fontWeight bold
-            marginLeft (px 12)
-            textDecoration none
-            textTransform uppercase
+  div # "#navigation" ? do
+    float floatRight
 
-base03  = rgb   0  43  54 
-base02  = rgb   7  54  66 
-base01  = rgb  88 110 117 
-base00  = rgb 101 123 131 
-base0   = rgb 131 148 150 
-base1   = rgb 147 161 161 
-base2   = rgb 238 232 213 
-base3   = rgb 253 246 227 
+  div # "#content" ? do
+    background  base3
+    border      solid (px 3) base1
+    posizionaOrizzontalmente
+    marginBottom (px 30)
 
--- yellow    181 137   0 
--- orange    203  75  22 
--- red       220  50  47 
--- magenta   211  54 130 
--- violet    108 113 196 
--- blue       38 139 210 
--- cyan       42 161 152 
--- green     133 153   0 
+  div # "#footer" ? do
+    marginTop (px 30)
+    background  base3
+    border      solid (px 3) base1
+    posizionaOrizzontalmente
+    marginBottom (px 30)
+
+
+-- Dark background colors
+base03  = rgb   0  43  54
+base02  = rgb   7  54  66
+-- Emphasized content
+base01  = rgb  88 110 117
+-- primary content
+base00  = rgb 101 123 131
+
+base0   = rgb 131 148 150
+-- comments, secondary content
+base1   = rgb 147 161 161
+
+-- Light background colors
+-- In light mode, is background highlight
+base2   = rgb 238 232 213
+-- In light mode, is background
+base3   = rgb 253 246 227
+
+
+-- Accent Colors
+mYellow  = rgb  181 137   0
+mOrange  = rgb  203  75  22
+mRed     = rgb  220  50  47
+mMagenta = rgb  211  54 130
+mViolet  = rgb  108 113 196
+mBlue    = rgb   38 139 210
+mCyan    = rgb   42 161 152
+mGreen   = rgb  133 153   0
+
+
+--------------------------------------------------------------------------------
+-- Funzioni di posizionamento orizzontale
+--------------------------------------------------------------------------------
+
+posizionaOrizzontalmente :: StyleM ()
+posizionaOrizzontalmente = do
+  marginLeft   (pct 12.5)
+  paddingLeft  (pct 5)
+  width        (pct 65)
+  paddingRight (pct 5)
+  marginRight  (pct 12.5)
